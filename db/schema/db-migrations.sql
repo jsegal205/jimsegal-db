a6df030691f29f3c49fb0a9eb2e8da800f2acfbb
Jim Segal <hello@jimsegal.com>
2020-02-23 21:20:46 -0600

    add recipes table

--- db/schema/db-migrations.sql
+++ db/schema/db-migrations.sql
@@ -1,3 +1,14 @@
 -- DB migrations
 
 ALTER TABLE IF EXISTS games ADD COLUMN "bgg_id" integer NOT NULL DEFAULT -1;
+
+
+-- create recipes table
+CREATE TABLE IF NOT EXISTS recipes
+(
+  id               serial PRIMARY KEY,
+  title            varchar(100) NOT NULL,
+  reference_link   varchar(255) NOT NULL,
+  ingredients      text NOT NULL,
+  directions       text NOT NULL
+);
